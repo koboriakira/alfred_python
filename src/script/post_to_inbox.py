d3b94c1c6a40549853f5ac93782b3830d5a0c1cb
a-kobori <89172056+a-kobori@users.noreply.github.com>
2025-12-30 19:35:54 +0900

    feat: Update post_to_inbox.py to post to Obsidian inbox and comment out Slack integration

--- src/script/post_to_inbox.py
+++ src/script/post_to_inbox.py
@@ -1,4 +1,5 @@
 import os
+from re import sub
 import sys
 
 sys.path.append(f"{os.environ['HOME']}/git/alfred_python/src/")
@@ -6,5 +7,12 @@ sys.path.append(f"{os.environ['HOME']}/git/alfred_python/src/")
 import config
 
 if __name__ == "__main__":
-    query = sys.argv[1]
-    config.post_to_inbox(query)
+    text = sys.argv[1]
+
+    # Slackのinboxチャンネルに投稿
+    # config.post_to_inbox(text)
+
+    # obsidianのinboxに追記
+    config.create_obsidian_markdown(
+        title=text, content="", subdir="00_Inbox"
+    )
